c8a787731137ced73174ffc8445b941144ec33e9
Alexander Repeta <alexander.repeta@gmail.com>
2024-09-25 18:33:36 +0300

    format

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'https://task-manager-api.goit.global/';
+axios.defaults.baseURL = "https://task-manager-api.goit.global/";
 
-const setAuthHeader = token => {
+const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 const clearAuthHeader = () => {
@@ -16,15 +16,18 @@ const clearAuthHeader = () => {
  *
  * After successful registration, add the token to the HTTP header
  */
-export const register = createAsyncThunk('auth/register', async (credentials, thunkApi) => {
-  try {
-    const { data } = await axios.post('/users/signup', credentials);
-    setAuthHeader(data.token);
-    return data;
-  } catch (error) {
-    return thunkApi.rejectWithValue(error.message);
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post("/users/signup", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
   }
-});
+);
 
 /*
  * POST @ /users/login
@@ -32,15 +35,18 @@ export const register = createAsyncThunk('auth/register', async (credentials, th
  *
  * After successful login, add the token to the HTTP header
  */
-export const logIn = createAsyncThunk('auth/login', async (credentials, thunkApi) => {
-  try {
-    const { data } = await axios.post('users/login', credentials);
-    setAuthHeader(data.token);
-    return data;
-  } catch (error) {
-    return thunkApi.rejectWithValue(error.message);
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post("users/login", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
   }
-});
+);
 
 /*
  * POST @ /users/logout
@@ -48,9 +54,9 @@ export const logIn = createAsyncThunk('auth/login', async (credentials, thunkApi
  *
  * After a successful logout, remove the token from the HTTP header
  */
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+export const logOut = createAsyncThunk("auth/logout", async (_, thunkApi) => {
   try {
-    await axios.post('users/logout');
+    await axios.post("users/logout");
     clearAuthHeader();
   } catch (error) {
     return thunkApi.rejectWithValue(error.message);
